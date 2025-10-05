61c3887c376a8477b0b8b89f7e34e13f6a251754
VladimirTensh <kitkatorgnew@gmail.com>
2025-10-05 19:42:34 +0300

    add global exception handler

--- src/main/java/com/project/event_notification/web/AccessDeniedExceptionHandler.java
+++ src/main/java/com/project/event_notification/web/AccessDeniedExceptionHandler.java
@@ -1,16 +1,31 @@
 package com.project.event_notification.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.OffsetDateTime;
 
 @Component
 public class AccessDeniedExceptionHandler implements AccessDeniedHandler {
+
+    private final Logger log = LoggerFactory.getLogger(AuthenticationEntryPointHandler.class);
+
+    private final ObjectMapper objectMapper;
+
+    public AccessDeniedExceptionHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void handle(
             HttpServletRequest request,
@@ -18,5 +33,16 @@ public class AccessDeniedExceptionHandler implements AccessDeniedHandler {
             AccessDeniedException accessDeniedException
     ) throws IOException, ServletException {
 
+        log.info("Got access denied exception");
+        var messageError = new ServerErrorDto(
+                "Access denied",
+                accessDeniedException.getMessage(),
+                OffsetDateTime.now()
+        );
+
+        var json = objectMapper.writeValueAsString(messageError);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(json);
     }
 }
